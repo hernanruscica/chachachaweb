4f2e5388e63cc7652b3f85f2206481f3cc8ff073
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-05-16 17:07:54 -0300

    Add files via upload
    Empecé a hacer las inserciones provisorias de las tablas sin foreign keys

---
+++ db/chachacha_db_inserciones_ACTORES.sql
@@ -0,0 +1,62 @@
+/* Insercion de un registro en la tabla "actores"  fechas en formato dateTime
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'varchar100', 'Text', 'varchar50', 'aaaa-mm-dd hh:mm:ss', 'aaaa-mm-dd hh:mm:ss');
+*/
+/*En el caso de no tener fecha de nacimiento o de fallecimiento, se pone '1900-01-01 00:00:00'*/
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Diego Esteban Capusotto', 'Actor argentino hincha de Racing', 'capusotto01.png', '1961-09-21 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Fabio Diego Alberti', 'Actualmente en choto.uy', 'alberti01.png', '1964-09-22 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Alfredo Casero', 'Casero Experimento', 'casero01.png', '1962-11-12 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Rodolfo Samso', 'Humorista en mundoAlacran.com', 'samso01.png', '1960-01-24 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Pablo Cedron', 'Actor fundador de Cha Cha Cha', 'cedron01.png', '1958-01-07 00:00:00', '2017-11-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Mex Urtizberea', 'Actor y musico argentino', 'urtizberea01.png', '1960-10-25 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Vivian El Jaber', 'Actriz argentina', 'eljaber01.png', '1964-11-17 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Mariana Briski', 'Humorista argentina', 'briski01.png', '1965-09-14 00:00:00', '2014-08-14 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Sandra Monteagudo', 'Actriz argentina', 'monteagudo01.png', '1900-01-01 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Alejandra Flechner', 'Es una actriz argentina de teatro, cine y televisión.​ ', 'flechner01.png', '1961-11-04 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Gisela Gaeta', 'Actriz argentina de televisión.​ ', 'gaeta01.png', '1900-01-01 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Daniel Marin', 'Actor y musico argentino ', 'marin01.png', '1964-09-04 00:00:00', '2016-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Martin Pavlovsky', 'Actor argentino ', 'pavlovsky01.png', '1962-04-16 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Jorge Takashima', 'Actor argentino', 'takashima01.png', '1900-01-01 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Santiago Rios', 'Actor argentino', 'rios01.png', '1900-01-01 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Claudia Pipi Disti', 'Actriz argentina', 'disti01.png', '1900-01-01 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Lito Ming', 'Actor argentino', 'ming01.png', '1900-01-01 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Romina Sznaider', 'Actriz argentina', 'sznaider01.png', '1900-01-01 00:00:00', '1900-01-01 00:00:00');
+
+INSERT INTO `actores` (`id`, `nombre`, `descripcion`, `captura_enlace`, `fecha_nacimiento`, `fecha_fallecimiento`) 
+VALUES (NULL, 'Iván Moschner', 'Actor argentino', 'moschner01.png', '1900-01-01 00:00:00', '1900-01-01 00:00:00');
---
+++ db/chachacha_db_inserciones_TEMPORADAS.sql
@@ -0,0 +1,18 @@
+
+/* Insercion de un registro en la tabla "Temporadas"  
+INSERT INTO `temporadas` (`id`, `nombre`, `descripcion`, `apertura`, `cierre`) 
+VALUES (NULL, 'varchar100', 'varchar100', 'varchar100', 'varchar100');
+apertura: 
+cierre:
+*/
+INSERT INTO `temporadas` (`id`, `nombre`, `descripcion`, `apertura`, `cierre`) 
+VALUES (NULL, 'Cha Cha Cha', 'Primer temporada de Cha Cha Cha.', 'Mozart avec nous de Boris Vian', 'Mozart avec nous de Boris Vian');
+
+INSERT INTO `temporadas` (`id`, `nombre`, `descripcion`, `apertura`, `cierre`) 
+VALUES (NULL, 'Dancing en el Titanic', 'Segunda temporada de Cha Cha Cha.', 'Mozart avec nous de Boris Vian', 'La Halibour Fiberglass Sereneiders');
+
+INSERT INTO `temporadas` (`id`, `nombre`, `descripcion`, `apertura`, `cierre`) 
+VALUES (NULL, 'El estigma del Dr. Vaporeso', 'Tercer temporada de Cha Cha Cha.', 'Marcha del Dr. Vaporeso de Alfredo Casero', 'Bailando en la Sociedad Rural de Alfredo Casero');
+
+INSERT INTO `temporadas` (`id`, `nombre`, `descripcion`, `apertura`, `cierre`) 
+VALUES (NULL, 'La parrilla del xeñor', 'Cuarta temporada de Cha Cha Cha.', '"Canon" de Johann Pachelbel', '3er acto de la sinfonía Solomon, HWV.67 de George Frideric Handel, y "Los Orozco" de León Gieco.');
