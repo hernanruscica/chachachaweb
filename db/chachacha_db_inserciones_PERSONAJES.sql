ea78b121842a089f524571a1cac2d0da96e15e9f
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-05-17 16:12:31 -0300

    agregue 2 inserciones de tablas de prueba

---
+++ db/chachacha_db_inserciones_PERSONAJES.sql
@@ -0,0 +1,19 @@
+/* Insercion de un registro en la tabla "Personajes"  - El ultimo campo es el index de un sketch tipo
+INSERT INTO `personajes` (`id`, `nombre`, `descripcion`, `captura_enlace`, `id_sketch_tipo`) 
+VALUES (NULL, 'varchar100', 'Text', 'varchar50', '2');
+*/
+
+INSERT INTO `personajes` (`id`, `nombre`, `descripcion`, `captura_enlace`, `id_sketch_tipo`) 
+VALUES (NULL, 'Curita', 
+'El curita de "Todos juntos en capilla. Nos cuenta las enseñanzas de Peperino Pomoro', 
+'curita01.png', '1');
+
+INSERT INTO `personajes` (`id`, `nombre`, `descripcion`, `captura_enlace`, `id_sketch_tipo`) 
+VALUES (NULL, 'Juan Carlos Batman', 
+'Nuestro batman argentino.', 
+'jcbatman01.png', '2');
+
+INSERT INTO `personajes` (`id`, `nombre`, `descripcion`, `captura_enlace`, `id_sketch_tipo`) 
+VALUES (NULL, 'Gilberto Manhattan Ruiz', 
+'Nuestro ministro de ahorro postal.', 
+'manhattanruiz01.png', '3');
---
+++ db/chachacha_db_inserciones_SKETCHES_TIPOS.sql
@@ -0,0 +1,55 @@
+/* Insercion de un registro en la tabla "Sketches_tipo"  - tipos de sketches - descripcion general
+INSERT INTO `sketches_tipos` (`id`, `nombre`, `descripcion`, `captura_enlace`) 
+VALUES (NULL, 'varchar100', 'text', 'varchar50');
+*/
+
+
+INSERT INTO `sketches_tipos` (`id`, `nombre`, `descripcion`, `captura_enlace`) 
+VALUES (NULL, 'Todos juntos en capilla', 
+'El más polémico y recordado de la serie, en el cual Fabio Alberti interpretaba un párroco que narraba las parábolas del Mártir Peperino Pómoro, sus ridículos milagros y las enseñanzas de su "Peperino Pómoro Regulandum Regulatis".', 
+'todosjuntosencapilla01.png');
+
+INSERT INTO `sketches_tipos` (`id`, `nombre`, `descripcion`, `captura_enlace`) 
+VALUES (NULL, 'Juan Carlos Batman', 
+'La versión argentina de la serie de TV. Alfredo Casero es Juan Carlos Bátman.', 
+'juancarlosbatman01.png');
+
+INSERT INTO `sketches_tipos` (`id`, `nombre`, `descripcion`, `captura_enlace`) 
+VALUES (NULL, 'Gilberto Manhattan Ruiz', 
+'Casero es el Ministro de ahorro postal, que anuncia medidas de recorte y cachetazos económicos, generalmente muy alterado.', 
+'manhattanruiz01.png');
+
+INSERT INTO `sketches_tipos` (`id`, `nombre`, `descripcion`, `captura_enlace`) 
+VALUES (NULL, 'Siddharta Kiwi', 
+'Un hindú interpretado por Diego Capusotto, que enseña como "enyoguizarse" y responder a las agresiones o situaciones indeseables con las que uno se puede encontrar en la calle a diario.', 
+'siddhartakiwi01.png');
+
+INSERT INTO `sketches_tipos` (`id`, `nombre`, `descripcion`, `captura_enlace`) 
+VALUES (NULL, 'Fatigatti', 
+'Hugo Cosme Fatigatti (Capusotto) es un futbolista que vive siendo acusado por dos árbitros corruptos (Casero y Alberti).', 
+'fatigatti01.png');
+
+INSERT INTO `sketches_tipos` (`id`, `nombre`, `descripcion`, `captura_enlace`) 
+VALUES (NULL, 'Buscando nuevas voces', 
+'Casero participa de castings para locutor pero nunca lo toman porque se pone nervioso y mueve las orejas. Buscando nuevas voces: Casero participa de castings para locutor pero nunca lo toman porque se pone nervioso y mueve las orejas.', 
+'orejas01.png');
+
+INSERT INTO `sketches_tipos` (`id`, `nombre`, `descripcion`, `captura_enlace`) 
+VALUES (NULL, 'Los Cubrepileta', 
+'La vida de una familia italiana.', 
+'loscubrepileta01.png');
+
+INSERT INTO `sketches_tipos` (`id`, `nombre`, `descripcion`, `captura_enlace`) 
+VALUES (NULL, 'Telescuela Técnica', 
+'Programa donde Casero, con la ayuda de su asistente (Urtizberea en la primer temporada, Marín después.)', 
+'telescuelatecnica01.png');
+
+INSERT INTO `sketches_tipos` (`id`, `nombre`, `descripcion`, `captura_enlace`) 
+VALUES (NULL, 'La Señora Luna', 
+'Rodolfo Samsó interpreta a una mujer que cuenta historias a los niños para dormir. Las mismas comienzan con una canción infantil o un consejo pero terminan con un relato sangriento.)', 
+'señoraluna01.png');
+
+INSERT INTO `sketches_tipos` (`id`, `nombre`, `descripcion`, `captura_enlace`) 
+VALUES (NULL, 'Mañanas al pedo', 
+'Fabio Alberti es la conductora de un programa para amas de casa que se emitía a la madrugada', 
+'manianasalpedo01.png');
